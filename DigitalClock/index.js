a2311b61c451c8c4a4ef225978c359b8a58bef7e
Pushpendra Sharma <pushpendrasharmaa2015@gmail.com>
2024-01-28 22:32:21 +0530

    added js in digital clock

--- DigitalClock/index.js
+++ DigitalClock/index.js
@@ -1,1 +1,35 @@
 console.log("This is the javascript file...");
+const hourElement = document.getElementById("hours");
+const minuteElement = document.getElementById("minutes");
+const secondElement = document.getElementById("seconds");
+const ampmElement = document.getElementById("ampm");
+
+console.log({
+  hourElement: hourElement,
+  minuteElement: minuteElement,
+  secondElement: secondElement,
+  ampm: ampm,
+});
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourElement.innerText = h;
+  minuteElement.innerText = m;
+  secondElement.innerText = s;
+  ampmElement.innerText = ampm;
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+updateClock();
